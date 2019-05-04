827d7bfe5b794f2af5d30c219a9975bc23380d67
lucas.rocha <lucas.rocha@talkeen.com.br>
2019-05-04 16:47:50 -0300

    Alteracao de mais alguns schemas

--- config/mongodb.js
+++ config/mongodb.js
@@ -25,43 +25,89 @@ const run = async () => {
     autoReconnect: true
   });
 }
+
+
+//? coloquei esses schemas aqui mas ta errado, acredito que deveriam estar la nos models, tenho que definir os exports de acordo com os chamados dos DAO. Veremos...
+
 const Schema = mongoose.Schema;
 
 const campaignSchema = Schema({
   name: String,
   description: String,
-  mailings: [],
+  mailings: [Schema.Types.ObjectId],
+  company: Schema.Types.ObjectId,
+  confDefault: Schema.Types.ObjectId,
+  type: String,
 });
 
 const mailingSchema = Schema({
   name: String,
-  contact: {
-    nome: String,
+  contact: [{
+    name: String,
     cpf: Number,
     phone: [Number],
-    binded: {type: Boolean, default: false},
+    binded: {
+      type: Boolean,
+      default: false
+    },
     wchecked: {
       dateChecked: Date,
-      verified: {type: Boolean, default: false}
+      verified: {
+        type: Boolean,
+        default: false
+      }
     },
-    status_env: {
+    statusSend: {
       type: Number,
       default: 0
-    } //aqui poderia ser do tipo status_env que é outro schema
-  },
-  mDate: {
+    }, //aqui poderia ser do tipo status_env que é outro schema
+    keyfield: [],
+  }],
+  mailingDate: {
     type: Date,
     default: Date.now
   },
-  _mailingId: Schema.Types.ObjectId,
-  amont: {
-    type: Number,
+  amont: Number, //aqui eu tenho que fazer uma função pra verficar a quantidade de contacts, pode ser contact.lenght
+  message: {
+    mTxt: String,
+    nameKey: {
+      type: Boolean,
+      default: true
+    },
+  },
+  status: {type: Number, default: 0},
+  campaign: Schema.Types.ObjectId
+});
+
+const dispatchConfig = Schema({
+  queue: Number,
+  description: String,
+  cadence: Number,
+  initDt: Date,
+  endDt: Date,
+  internalNumbers: {
+    bool: {
+      type: Boolean,
+      default: false
+    },
+    numbers: [Numbers]
+  },
+  mailings: [Schema.Types.ObjectId],
+})
+
+const company = Schema({
+  name: String,
+  description: String,
+  active: Boolean,
+  credits: Number,
+  campaignMax: Number,
+  simultaneosSend: Number,
+  manualSend: {
+    type: Boolean,
+    default: false
   }
 });
 
-const envio = Schema({
-
-})
 
 module.exports = {
   Run: run,
