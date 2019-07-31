e80fee9a6c20792ea7175521a52f4ecffbb396e3
lucashenrique_18 <lucashenrique_18@outlook.com>
2019-07-31 19:41:19 -0300

    DEBUG mLab

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 
 const uri = `mongodb://${process.env.USERN}:${process.env.PASS}@${process.env.HOST}:${process.env.PORT}/${process.env.DBNAME}`;
 
-console.log(uri);
-
 mongoose.Promise = Promise
 
 mongoose.connection.on('connected', () => {
