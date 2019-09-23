22d5abef23aa9c5c226adf302c40a3ed7997c19d
lucashenrique_18 <lucashenrique_18@outlook.com>
2019-09-23 16:40:26 -0300

    Correção controller login

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -6,7 +6,6 @@ module.exports.login = async function(app, req, res){
 	const data = req.body;
 	const db = await app.config.mongodb;
 
-	console.log(req.decoded)
 	db.Run()
 		.then( async () => {
 			const loginDAO = new app.api.models.loginDAO(db);
