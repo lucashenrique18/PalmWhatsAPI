0742030ef3601607075b6543451a485c646e2cdf
lucashenrique_18 <lucashenrique_18@outlook.com>
2019-10-02 20:04:55 -0300

    Inclusão do CORS

--- config/config.js
+++ config/config.js
@@ -1,14 +1,19 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
+    cors = require('cors'),
     consign = require('consign');
 
 var app = express();
+
+// utilizando o cors
+app.use(cors({origin: 'http://localhost:3000'}));
 
 //body-parser
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
 
 consign()
     .include('./config/mongodb.js')
